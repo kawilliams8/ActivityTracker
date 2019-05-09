de8c7e1559c38f70a957d9b690ab3b4e2df2c329
Steve Rumizen <steverumizen@gmail.com>
2019-05-08 18:16:24 -0600

    Add basic dom manipulation > > > Co-authored-by: Katie Williams <kawilliams8@gmail.com> > Co-authored-by: Steve Rumizen <steverumizen@gmail.com>

--- src/User.js
+++ src/User.js
@@ -1,12 +1,6 @@
 class User {
 	constructor(userData) {
 		this.userData = userData;
-		// this.id = userData.id;
-		// this.name = userData.name;
-		// this.address = userData.address;
-		// this.email = userData.email;
-		// this.strideLength = userData.strideLength;
-		// this.dailyStepGoal = userData.dailyStepGoal;
 	}
 
 	returnFirstName() {
