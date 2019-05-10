cec899d499bba33197600de895977824b04cd6b6
Steve Rumizen <steverumizen@gmail.com>
2019-05-09 18:41:33 -0600

    Add sleep methods and testing Co-authored-by: Katie Williams <kawilliams8@gmail.com>

--- tests/Sleep-test.js
+++ tests/Sleep-test.js
@@ -1,16 +1,28 @@
 const chai = require('chai');
 const expect = chai.expect;
 
-const UserRepository = require('../src/UserRepository')
+const SleepRepository = require('../src/SleepRepository')
+const Sleep = require('../src/Sleep')
 
-describe('UserRepository', function() {
+describe('Sleep', function() {
 
 	it('Should be a function', function() {
-		expect(UserRepository).to.be.a('function');
+		expect(Sleep).to.be.a('function');
 	});
 
-	it('Should be an instance of the User class', function() {
-		const userRepository = new UserRepository();
-		expect(userRepository).to.be.an.instanceOf(UserRepository);
+	it('Should be an instance of the Sleep class', function() {
+		const sleep = new Sleep();
+		expect(sleep).to.be.an.instanceOf(Sleep);
 	});
-})
+
+	it('Should return how many hours a user slept on average per day', function() {
+		const sleepRepo = new SleepRepository('../data/proxy-sleep');
+		expect(sleepRepo.returnSleep(2).returnAvgHours()).to.eql(7)
+	});
+
+	it('Should return a users all time average sleep quality', function() {
+		const sleepRepo = new SleepRepository('../data/proxy-sleep');
+		expect(sleepRepo.returnSleep(3).returnAllTimeQuality()).to.eql(3)
+	});
+
+});
