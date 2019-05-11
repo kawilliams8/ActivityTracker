e27058a20a50828a4605c1c55088288993f4d100
Steve Rumizen <steverumizen@gmail.com>
2019-05-11 16:12:36 -0600

    Refactor sleepRepo method Co-authored-by: Katie Williams <kawilliams8@gmail.com>

--- src/SleepRepository.js
+++ src/SleepRepository.js
@@ -26,93 +26,15 @@ class SleepRepository {
 	}
 
 	returnLongestSleepers(givenDate) {
-		const data = [
-  {
-    "userID": 1,
-    "sleepData": [
-      {
-        "date": "06/05/2019",
-        "hoursSlept": 8,
-        "sleepQuality": 4.8
-      },
-      {
-        "date": "07/05/2019",
-        "hoursSlept": 10.7,
-        "sleepQuality": 4.8
-      },
-      {
-        "date": "08/05/2019",
-        "hoursSlept": 8.1,
-        "sleepQuality": 1.9
-      }]
-  },
-  {
-    "userID": 2,
-    "sleepData": [
-      {
-        "date": "06/05/2019",
-        "hoursSlept": 6.3,
-        "sleepQuality": 2.2
-      },
-      {
-        "date": "07/05/2019",
-        "hoursSlept": 9.2,
-        "sleepQuality": 4.8
-      },
-      {
-        "date": "08/05/2019",
-        "hoursSlept": 6.6,
-        "sleepQuality": 4.5
-      }]
-  },
-  {
-    "userID": 3,
-    "sleepData": [
-      {
-        "date": "06/05/2019",
-        "hoursSlept": 7.2,
-        "sleepQuality": 5
-      },
-      {
-        "date": "07/05/2019",
-        "hoursSlept": 5.4,
-        "sleepQuality": 4.1
-      },
-      {
-        "date": "08/05/2019",
-        "hoursSlept": 4.7,
-        "sleepQuality": 1.2
-      }]
-  },
-  {
-    "userID": 4,
-    "sleepData": [
-      {
-        "date": "06/05/2019",
-        "hoursSlept": 8,
-        "sleepQuality": 4.8
-      },
-      {
-        "date": "07/05/2019",
-        "hoursSlept": 10.7,
-        "sleepQuality": 4.8
-      },
-      {
-        "date": "08/05/2019",
-        "hoursSlept": 8.1,
-        "sleepQuality": 1.9
-      }]
-  }
-]	
-		const reduced = data.reduce((acc, user) => {
-			const matchingDate = user.sleepData.filter(date => date.date === givenDate);
-			acc.push({id: user.userID, ...matchingDate })
+		const reduced = this.instantiateSleeps().reduce((acc, user) => {
+			let matchingDate = user.userSleepData.sleepData.filter(day => day.date === givenDate);
+			matchingDate = { id: user.userSleepData.userID, ...matchingDate };
+			acc.push(matchingDate);
 			return acc;
 		}, [])
-
 		reduced.sort((a, b) => b['0'].hoursSlept - a['0'].hoursSlept);
 		const result = reduced.filter(item => item['0'].hoursSlept === reduced[0]['0'].hoursSlept)
-		console.log(result.map(item => item.id));
+		return result.map(item => item.id);
 	}
 
 
